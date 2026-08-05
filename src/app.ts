026b595f8212a1a7dbadefd5c12e30fb54fb8223
Kawser Simanto <kawsersimanto@gmail.com>
2026-08-06 01:07:20 +0600

    chore: removed unused prisma query

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import express, {
   type Request,
   type Response,
 } from "express";
-import { prisma } from "./app/lib/prisma";
 
 export const app: Express = express();
 
@@ -13,16 +12,9 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 
 app.get("/", async (req: Request, res: Response) => {
-  const specialty = await prisma.specialty.create({
-    data: {
-      title: "Test",
-    },
-  });
-
   res.status(200).json({
     success: true,
-    message: "Data created successfully",
-    data: specialty,
+    message: "Server Running",
   });
 });
 
