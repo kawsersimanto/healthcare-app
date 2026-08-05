9f2b65791d3d13aaecc01ebbf38e42c97c21c138
Kawser Simanto <kawsersimanto@gmail.com>
2026-08-06 02:06:48 +0600

    chore: added controller and routes

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,12 @@
-import express, {
-  json,
-  urlencoded,
-  type Express,
-  type Request,
-  type Response,
-} from "express";
+import express, { Express, json, Request, Response, urlencoded } from "express";
+import { IndexRoutes } from "./app/routes";
 
-export const app: Express = express();
+const app: Express = express();
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
+
+app.use("/api/v1", IndexRoutes);
 
 app.get("/", async (req: Request, res: Response) => {
   res.status(200).json({
