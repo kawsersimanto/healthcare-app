c067c2f3547a2f6476852a92b356cf21055ae052
Kawser Simanto <kawsersimanto@gmail.com>
2026-08-06 00:01:01 +0600

    chore: added basic configuration for express server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,24 @@
-import express, { type Express, type Request, type Response } from "express";
+import dotenv from "dotenv";
+import express, {
+  json,
+  urlencoded,
+  type Express,
+  type Request,
+  type Response,
+} from "express";
+
+dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
